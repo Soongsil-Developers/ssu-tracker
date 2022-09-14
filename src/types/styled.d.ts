a099abc072e067e48fc02b3f3fdc799a00027d28
박준서 <oponize@naver.com>
2022-09-14 13:16:15 +0900

    chore: 피그마 디자인으로 변경

--- src/types/styled.d.ts
+++ src/types/styled.d.ts
@@ -9,13 +9,32 @@ declare module "styled-components" {
       white: string;
     };
     fontSize: {
-      m1: string;
-      b1: string;
-      b2: string;
-      // * Extra Bold
-      eb1: string;
-      eb2: string;
-      eb3: string;
+      title_mobile: string;
+      title_pc: string;
+      subtitle1_mobile: string;
+      subtitle1_pc: string;
+      subtitle2b_mobile: string;
+      subtitle2b_pc: string;
+      subtitle2r_mobile: string;
+      subtitle2r_pc: string;
+      subtitle3_mobile: string;
+      subtitle3_pc: string;
+      body1b_mobile: string;
+      body1b_pc: string;
+      body1r_mobile: string;
+      body1r_pc: string;
+      body2b_mobile: string;
+      body2b_pc: string;
+      body2r_mobile: string;
+      body2r_pc: string;
+      body3_mobile: string;
+      body3_pc: string;
+      caption1_mobile: string;
+      caption1_pc: string;
+      caption2r_mobile: string;
+      caption2r_pc: string;
+      caption2l_mobile: string;
+      caption2l_pc: string;
     };
     fontWeight: {
       extraBold: number;
