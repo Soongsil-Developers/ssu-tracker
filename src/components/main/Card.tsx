83420d63cdd397a303eefdfa5a91b753f6171212
nijuy <youjin6325@naver.com>
2022-09-15 12:32:47 +0900

    feat: show the results according to the option

--- src/components/main/Card.tsx
+++ src/components/main/Card.tsx
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Typography from "../typography";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+import { format } from "date-fns";
+import theme from "../../constants/theme";
+import DEVICE_LIST from "../../constants/device";
+import { applyMediaQuery } from "../../styles/mediaQuery";
+
+const responseNameFonts = {
+  mobile: theme.fontSize.body2b_mobile,
+  tablet: theme.fontSize.body2b_mobile,
+  wideTablet: theme.fontSize.body2b_mobile,
+  desktop: theme.fontSize.body2b_pc,
+  wideDesktop: theme.fontSize.body2b_pc,
+};
 
 const CardTemplate = styled.div`
   & {
@@ -20,53 +32,92 @@ const CardTemplate = styled.div`
 `;
 
 const Card = () => {
+  const data = [
+    {
+      inOut: "in",
+      date: new Date(2022, 8, 15, 16, 0),
+      type: "중앙도서관",
+    },
+    {
+      inOut: "in",
+      date: new Date(2022, 8, 15, 17, 30),
+      type: "정보과학관",
+    },
+  ];
+  const buildings = [
+    "모든 건물",
+    "중앙도서관",
+    "정보과학관",
+    "베어드홀",
+    "숭덕경상관",
+    "문화관",
+    "안익태기념관",
+    "형남공학관",
+    "교육관",
+    "백마관",
+    "신양관",
+  ];
+
+  const [filteredData, setFilteredData] = useState(data);
+  const getFilterData = (option: string) => {
+    if (option === "모든 건물") setFilteredData(data);
+    else
+      setFilteredData(
+        data.filter((log: { type: string }) => log.type === option)
+      );
+  };
+
+  const changeOption = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    getFilterData(e.target.value);
+  };
+
   return (
     <div style={{ border: "1px solid blue" }}>
+      <form>
+        <select onChange={changeOption}>
+          {buildings.map((building) => (
+            <option value={building}> {building} </option>
+          ))}
+        </select>
+      </form>
+
       <div>
-        <CardTemplate>
-          <div>
-            <Typography as="p" size="m1" weight="semiBold" lineHeight={1.06}>
-              입장
+        {filteredData.map((d: { inOut: string; date: Date; type: string }) => (
+          <CardTemplate>
+            <div>
+              <Typography
+                as="p"
+                weight="semiBold"
+                lineHeight={1.06}
+                css={responsiveNameCss}
+              >
+                {d.inOut === "in" ? "입장" : "퇴장"}
+              </Typography>
+              <Typography
+                as="p"
+                weight="semiBold"
+                color="gray_5"
+                css={responsiveNameCss}
+              >
+                {format(d.date, "LL.dd kk:mm")}
+              </Typography>
+            </div>
+            <Typography as="p" weight="semiBold" css={responsiveNameCss}>
+              {d.type}
             </Typography>
-            <Typography as="p" size="m1" weight="semiBold" color="gray">
-              09.12 02:00
-            </Typography>
-          </div>
-          <Typography as="p" size="m1" weight="semiBold">
-            정보과학관
-          </Typography>
-        </CardTemplate>
-
-        <CardTemplate>
-          <div>
-            <Typography as="p" size="m1" weight="semiBold">
-              입장
-            </Typography>
-            <Typography as="p" size="m1" weight="semiBold" color="gray">
-              09.12 02:00
-            </Typography>
-          </div>
-          <Typography as="p" size="m1" weight="semiBold">
-            정보과학관
-          </Typography>
-        </CardTemplate>
-
-        <CardTemplate>
-          <div>
-            <Typography as="p" size="m1" weight="semiBold">
-              입장
-            </Typography>
-            <Typography as="p" size="m1" weight="semiBold" color="gray">
-              09.12 02:00
-            </Typography>
-          </div>
-          <Typography as="p" size="m1" weight="semiBold">
-            정보과학관
-          </Typography>
-        </CardTemplate>
+          </CardTemplate>
+        ))}
       </div>
     </div>
   );
 };
+
+const responsiveNameCss = css`
+  ${DEVICE_LIST.map(
+    (device) => `${applyMediaQuery(device)} {
+    font-size: ${responseNameFonts[device]}
+  }`
+  ).join("")}
+`;
 
 export default Card;
